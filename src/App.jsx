9f49cbae049f20e91a4e434b1aae5ae5117dd488
sammothxc <sdwarrior8@gmail.com>
2024-05-23 02:32:21 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
 import Typewriter from 'typewriter-effect';
-import mainbl from './assets/mainbl.svg'
-import webdes from '/webdes.jpg'
+import mainbl from '/mainbl.svg'
+import footerimg from '/footerimg.svg'
 import './App.css'
 
 function Menu({ toggleTheme, theme }) {
@@ -23,7 +23,7 @@ function Menu({ toggleTheme, theme }) {
     contactUsSection.scrollIntoView({ behavior: 'smooth' });
   };
   return (
-    <header className="sticky-header">
+    <header className='sticky-header'>
       <nav>
         <li>
           <a className='draw-border' onClick={scrollToHome}>
@@ -48,15 +48,15 @@ function Menu({ toggleTheme, theme }) {
         <li className='toggle-container'>
           <div>Dark Mode /<p className='outline'>/</p></div>
           <input
-            type="checkbox"
-            id="theme-toggle"
-            className="theme-toggle-checkbox"
+            type='checkbox'
+            id='theme-toggle'
+            className='theme-toggle-checkbox'
             checked={theme === 'dark'}
             onChange={toggleTheme}
           />
           <label
-            htmlFor="theme-toggle"
-            className="theme-toggle-label">
+            htmlFor='theme-toggle'
+            className='theme-toggle-label'>
           </label>
         </li>
       </nav>
@@ -71,13 +71,13 @@ function Home() {
   };
 
   return (
-    <section id='Home' className='section-end'>
+    <section id='Home' className='section'>
       <div>
         <div>
           <img src={mainbl} className='svgs dark' />
         </div>
       </div>
-      <div className="terminal-container">
+      <div className='terminal-container'>
         <div className='terminal mono'>
           <Typewriter
             className='typewriter'
@@ -85,7 +85,7 @@ function Home() {
               autoStart: true,
               loop: true,
               delay: 50,
-              deleteSpeed: 50,
+              deleteSpeed: 20,
               cursor: '█',
             }}
             onInit={(typewriter) => {
@@ -123,11 +123,11 @@ function WhatWeDo() {
   }, []);
 
   return (
-    <section id='WhatWeDo' className='section-end'>
+    <section id='WhatWeDo' className='section'>
       <h1>What We Do /<p className='outline o1'>/</p></h1>
-      <div className="skills-grid">
-        <div className="skill">
-          <div className="scrolling-background mono" id='scrolly'>
+      <div className='skills-grid'>
+        <div className='skill'>
+          <div className='scrolling-background mono' id='scrolly'>
             {rawHTML}
           </div>
           <h2>Web Development /<p className='outline o2'>/</p></h2>
@@ -149,7 +149,7 @@ function WhatWeDo() {
             </li>
           </div>
         </div>
-        <div className="skill">
+        <div className='skill'>
           <h2>Web Design /<p className='outline o2'>/</p></h2>
           <div className='wrpr mono'>
             <img src={`/webdes.jpg`} className='skill-image'/>
@@ -158,7 +158,7 @@ function WhatWeDo() {
             <li>3</li>
           </div>
         </div>
-        <div className="skill">
+        <div className='skill'>
           <h2>Other Services /<p className='outline o2'>/</p></h2>
           <div className='wrpr mono'>
             <li>Social Media Management: Facebook, Instagram</li>
@@ -192,7 +192,7 @@ function CompletedProjects() {
   ];
 
   return (
-    <section id='CompletedProjects'>
+    <section id='CompletedProjects' className='section'>
       <h1>Completed Projects /<p className='outline o1'>/</p></h1>
       <div className='projects-grid'>
         {projects.map((project, index) => (
@@ -226,11 +226,49 @@ function CompletedProjects() {
 function ContactUs() {
   return (
     <section id='ContactUs'>
-      <h1>Contact Us /<p className='outline o1'>/</p></h1>
-      <div className='contact-form mono'>
-        <p>
-          Send us a message and we'll get back to you as soon as possible.
-        </p>
+      <h1>Let's Start /<p className='outline o1'>/</p></h1>
+      <p className='mono'>
+        Send us a message or a quote and we'll get back to you as soon as possible!
+      </p>
+      <div id='contact-container'>
+        <div className='contact'>
+          <a 
+            href='mailto:contact@samwarrwebdev.com?subject=Inquiry'
+            className='mono email'
+          >
+            contact@samwarrwebdev.com
+          </a>
+        </div>
+        <div className='contact o0'>
+          <div>/<p className='outline o0'>/</p></div>
+        </div>
+        <div id='right' className='contact'>
+        <a className="cta">
+          <span>Get Quote</span>
+          <span>
+            <svg width="66px" height="43px" viewBox="0 0 66 43" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+              <g id="arrow" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                <path className="one" d="M40.1543933,3.89485454 L43.9763149,0.139296592 C44.1708311,-0.0518420739 44.4826329,-0.0518571125 44.6771675,0.139262789 L65.6916134,20.7848311 C66.0855801,21.1718824 66.0911863,21.8050225 65.704135,22.1989893 C65.7000188,22.2031791 65.6958657,22.2073326 65.6916762,22.2114492 L44.677098,42.8607841 C44.4825957,43.0519059 44.1708242,43.0519358 43.9762853,42.8608513 L40.1545186,39.1069479 C39.9575152,38.9134427 39.9546793,38.5968729 40.1481845,38.3998695 C40.1502893,38.3977268 40.1524132,38.395603 40.1545562,38.3934985 L56.9937789,21.8567812 C57.1908028,21.6632968 57.193672,21.3467273 57.0001876,21.1497035 C56.9980647,21.1475418 56.9959223,21.1453995 56.9937605,21.1432767 L40.1545208,4.60825197 C39.9574869,4.41477773 39.9546013,4.09820839 40.1480756,3.90117456 C40.1501626,3.89904911 40.1522686,3.89694235 40.1543933,3.89485454 Z" fill="#FFFFFF"></path>
+                <path className="two" d="M20.1543933,3.89485454 L23.9763149,0.139296592 C24.1708311,-0.0518420739 24.4826329,-0.0518571125 24.6771675,0.139262789 L45.6916134,20.7848311 C46.0855801,21.1718824 46.0911863,21.8050225 45.704135,22.1989893 C45.7000188,22.2031791 45.6958657,22.2073326 45.6916762,22.2114492 L24.677098,42.8607841 C24.4825957,43.0519059 24.1708242,43.0519358 23.9762853,42.8608513 L20.1545186,39.1069479 C19.9575152,38.9134427 19.9546793,38.5968729 20.1481845,38.3998695 C20.1502893,38.3977268 20.1524132,38.395603 20.1545562,38.3934985 L36.9937789,21.8567812 C37.1908028,21.6632968 37.193672,21.3467273 37.0001876,21.1497035 C36.9980647,21.1475418 36.9959223,21.1453995 36.9937605,21.1432767 L20.1545208,4.60825197 C19.9574869,4.41477773 19.9546013,4.09820839 20.1480756,3.90117456 C20.1501626,3.89904911 20.1522686,3.89694235 20.1543933,3.89485454 Z" fill="#FFFFFF"></path>
+                <path className="three" d="M0.154393339,3.89485454 L3.97631488,0.139296592 C4.17083111,-0.0518420739 4.48263286,-0.0518571125 4.67716753,0.139262789 L25.6916134,20.7848311 C26.0855801,21.1718824 26.0911863,21.8050225 25.704135,22.1989893 C25.7000188,22.2031791 25.6958657,22.2073326 25.6916762,22.2114492 L4.67709797,42.8607841 C4.48259567,43.0519059 4.17082418,43.0519358 3.97628526,42.8608513 L0.154518591,39.1069479 C-0.0424848215,38.9134427 -0.0453206733,38.5968729 0.148184538,38.3998695 C0.150289256,38.3977268 0.152413239,38.395603 0.154556228,38.3934985 L16.9937789,21.8567812 C17.1908028,21.6632968 17.193672,21.3467273 17.0001876,21.1497035 C16.9980647,21.1475418 16.9959223,21.1453995 16.9937605,21.1432767 L0.15452076,4.60825197 C-0.0425130651,4.41477773 -0.0453986756,4.09820839 0.148075568,3.90117456 C0.150162624,3.89904911 0.152268631,3.89694235 0.154393339,3.89485454 Z" fill="#FFFFFF"></path>
+              </g>
+            </svg>
+          </span> 
+        </a>
+        </div>
+      </div>
+      <div id='links-container'>
+        <div id='footer-img'>
+          <img id='footerimg' src={footerimg} className='svgs dark'/>
+        </div>
+        <div className='links'>
+          <li><a href='https://samwarrwebdev.com'>Copyright Notice</a></li>
+          <li><a href='https://samwarrwebdev.com'>Privacy Policy Link</a></li>
+          <li><a href='https://samwarrwebdev.com'>Sitemap</a></li>
+          <li><a href='https://samwarrwebdev.com'>Logo</a></li>
+          <li><a href='https://samwarrwebdev.com'>CP Info</a></li>
+          <li><a href='https://samwarrwebdev.com'>Github</a></li>
+        </div>
       </div>
     </section>
   )
